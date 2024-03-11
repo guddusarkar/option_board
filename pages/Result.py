9b6117cae1aed2a751d14a2d61b9e821e1a0ddb7
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2024-03-11 06:21:30 +0530

    Delete pages directory

--- pages/Result.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-import pandas as pd
-st.title('announcement of result')
-d=pd.read_html("https://economictimes.indiatimes.com/markets/stocks/mcalendar.cms")
-st.dataframe(d[0])
