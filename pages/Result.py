161faff5cc67f2aa17acd2dc2008093333d5fd3f
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2024-03-12 23:40:52 +0530

    Update Result.py

--- pages/Result.py
+++ pages/Result.py
@@ -9,6 +9,6 @@ st.title(':red[Quaterly Result] for :blue[Today]')
 # extracting todays result announcement data from economic-times website 
 try:
   d=pd.read_html("https://economictimes.indiatimes.com/markets/stocks/mcalendar.cms")
-  st.table(d[0])
+  st.dataframe(d[0])
 except:
   st.text(' ** NOT RESULT ANNOUNCED TODAY**')
