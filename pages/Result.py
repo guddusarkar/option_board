9c22760318ff5d449af97afe49091195cf3c1025
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2024-03-12 23:55:19 +0530

    Update Result.py

--- pages/Result.py
+++ pages/Result.py
@@ -1,14 +1,36 @@
 # installing important libraries
 import streamlit as st
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
 # adding title of the page
 st.title(':red[Quaterly Result] for :blue[Today]')
 
+# extracting todays result announcement data from economic-times website using requests library
+url = "https://economictimes.indiatimes.com/markets/stocks/mcalendar.cms"
+response = requests.get(url)
 
-# extracting todays result announcement data from economic-times website 
-try:
-  d=pd.read_html("https://economictimes.indiatimes.com/markets/stocks/mcalendar.cms")
-  st.dataframe(d[0])
-except:
-  st.text(' ** NOT RESULT ANNOUNCED TODAY**')
+# use beautiful soup to extract table data
+soup = BeautifulSoup(response.content, 'html.parser')
+table = soup.find('table', class_="event_table")
+if table:
+    # Extract data from the table
+    rows = table.find_all('tr')
+    table_data = []
+
+    for row in rows:
+        columns = row.find_all(['td', 'th'])
+        row_data = [column.get_text(strip=True) for column in columns]
+        table_data.append(row_data)
+
+    # Remove any empty rows from the table data
+    table_data = [row for row in table_data if any(row)]
+
+    # Convert the table data into a DataFrame
+    df = pd.DataFrame(table_data[1:], columns=table_data[0])
+
+    # Print the DataFrame
+    st.table(df)
+else:
+    st.text(' ** NOT RESULT ANNOUNCED TODAY**')
