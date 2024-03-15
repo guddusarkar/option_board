9ce1160d276acab9aa57542b7843adb1f83fe5f8
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2024-03-16 02:17:25 +0530

    Update Financial.py

--- pages/Financial.py
+++ pages/Financial.py
@@ -20,18 +20,18 @@ with st.sidebar.form('formes'):
                 tables = pd.read_html(f"https://www.screener.in/company/{stock}/")
             with tab1:
                 st.subheader('Y-o-Y Balance sheet')
-                st.table(tables[6])
+                st.dataframe(tables[6], use_container_width= true)
             with tab2:
                 st.subheader('Y-o-Y Profit and Loss statment')
-                st.table(tables[1])
+                st.dataframe(tables[1], use_container_width= true)
             with tab3:
                 st.subheader('Q-o-Q Profit and Loss statment')
-                st.table(tables[0])
+                st.dataframe(tables[0], use_container_width= true)
             with tab4:
                 st.subheader('Y-o-Y Cash folw statment')
-                st.table(tables[7])
+                st.dataframe(tables[7], use_container_width= true)
             with tab5:
                 st.subheader('Owner Share holding Pattern')
-                st.table(tables[9])
+                st.dataframe(tables[9], use_container_width= true)
         except:
-            st.text('** please write correct symble name**')
+            st.text('please write correct symble')
