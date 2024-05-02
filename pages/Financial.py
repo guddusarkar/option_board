585b7196766cabb1636b4f5a2a28536a325c23dc
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2024-05-03 01:02:05 +0530

    Update Financial.py

--- pages/Financial.py
+++ pages/Financial.py
@@ -29,27 +29,27 @@ with st.sidebar.form('formes'):
                 st.subheader('Q-o-Q Profit and Loss statment')
                 qpl=st.dataframe(tables[0], use_container_width= True, hide_index=True)
                 #convart data into dataframe
-                qpl= pd.DataFrame(qpl.T)
-                qpl.drop(qpl.index[0], inplace=True)
-                qpl.columns= ['Sales', 'Expenses','Operating Profit', 'OPM%', 'Other income', 'Interest', 'Depreciation', 'PBT', 'Tax%','PAT','EPS','PDF']
-                qpl.drop(columns=['PDF','OPM%'],axis=1, inplace=True)
+                # qpl= pd.DataFrame(qpl.T)
+                # qpl.drop(qpl.index[0], inplace=True)
+                # qpl.columns= ['Sales', 'Expenses','Operating Profit', 'OPM%', 'Other income', 'Interest', 'Depreciation', 'PBT', 'Tax%','PAT','EPS','PDF']
+                # qpl.drop(columns=['PDF','OPM%'],axis=1, inplace=True)
 
-                #edit some columns
-                nu_col= ['Sales','Expenses','Operating Profit','Other income',	'Interest',	'Depreciation',	'PBT','PAT']
-                qpl[nu_col]=qpl[nu_col].astype('int')
-                qpl['Growth%']=((qpl.Sales/qpl.Sales.shift(1))-1)*100
-                qpl['OPM%']= (qpl['Operating Profit']/qpl.Sales)*100
+                # #edit some columns
+                # nu_col= ['Sales','Expenses','Operating Profit','Other income',	'Interest',	'Depreciation',	'PBT','PAT']
+                # qpl[nu_col]=qpl[nu_col].astype('int')
+                # qpl['Growth%']=((qpl.Sales/qpl.Sales.shift(1))-1)*100
+                # qpl['OPM%']= (qpl['Operating Profit']/qpl.Sales)*100
 
-                #ploting data
-                col1,col2= st.columns(2)
-                with col1:
-                    plt.figure(figsize=(12,4))
-                    plt.bar(x=qpl.index,height=qpl.Sales,width=0.5)
-                    plt.title('quaterly sales')
-                with col2:
-                    plt.figure(figsize=(12,4))
-                    plt.bar(x=qpl.index,height=qpl['Growth%'],width=0.5)
-                    plt.title('quaterly Sales Growth')
+                # #ploting data
+                # col1,col2= st.columns(2)
+                # with col1:
+                #     plt.figure(figsize=(12,4))
+                #     plt.bar(x=qpl.index,height=qpl.Sales,width=0.5)
+                #     plt.title('quaterly sales')
+                # with col2:
+                #     plt.figure(figsize=(12,4))
+                #     plt.bar(x=qpl.index,height=qpl['Growth%'],width=0.5)
+                #     plt.title('quaterly Sales Growth')
             with tab4:
                 st.subheader('Y-o-Y Cash folw statment')
                 st.dataframe(tables[7], use_container_width= True, hide_index=True)
