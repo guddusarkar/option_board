e156a8fa48f626730a5fb7136e784ecc0f094abc
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2024-09-19 02:50:59 +0530

    Create Holiday.py
    add holiday list

---
+++ pages/Holiday.py
@@ -0,0 +1,39 @@
+# import important libraries
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+import json
+
+#configur the page
+st.set_page_config(
+    page_title="sumancapz",
+    page_icon="💰",
+    layout="wide",
+    initial_sidebar_state="expanded")
+
+# input header of the page
+st.header("Annual :blue[Holiday] list :sunglasses:")
+
+# fatching data from NSE website
+header = {
+    "Connection": "keep-alive",
+    "Cache-Control": "max-age=0",
+    "DNT": "1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36 Edg/128.0.0.0",
+    "Sec-Fetch-User": "?1", "Accept": "*/*", "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "cors",
+    "Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9,hi;q=0.8"
+    }
+def nse_urlfetch(url):
+  r_session = requests.session()
+  nse_live = r_session.get("http://nseindia.com", headers=header)
+  return r_session.get(url, headers=header)
+try:
+  data= nse_urlfetch("https://www.nseindia.com/api/holiday-master?type=trading").json()
+  table= pd.DataFrame(data.get('CM'))
+table.columns= ['Date','Day','Description','SL no']
+table.drop(['SL no'],axis=1,inplace=True)
+st.dataframe(data= table,hide_index=True)
+except:
+  st.text('error to fatching data from site')
