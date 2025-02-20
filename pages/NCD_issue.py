77e268c781510c9135aafe4c476f7f24d9d69675
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2025-02-20 17:56:05 +0530

    Update NCD_issue.py

--- pages/NCD_issue.py
+++ pages/NCD_issue.py
@@ -41,9 +41,9 @@ if df.shape[0]>7:
     df= df[:7]
 else: df
 # display the table in streamlit page
-st.data_editor(df,hide_index=True,column_config={
+st.dataframe(df,hide_index=True,column_config={
     "URL":st.column_config.LinkColumn(
-            "Details", display_text="Link"),
+            "Details", display_text="Link")
 })
 # provide some details about NCD and selection process
 st.divider()
