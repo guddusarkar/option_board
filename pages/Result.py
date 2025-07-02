20a6fc3dc2537cf591c8495a1944c6b7c69c7ade
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2025-07-02 23:11:42 +0530

    Update Result.py
    Some alignment correction

--- pages/Result.py
+++ pages/Result.py
@@ -13,16 +13,17 @@ st.set_page_config(
 st.title(':red[Quaterly Result] for :blue[Today]')
 
 # extracting todays result announcement data from economic-times website using requests library
-url = "https://economictimes.indiatimes.com/markets/stocks/mcalendar.cms"
+economictimes = "https://economictimes.indiatimes.com/markets/stocks/mcalendar.cms"
+livemint='https://www.livemint.com/market/quarterly-results-calendar'
 # Fatch the data using pandas read_html
-table = pd.read_html(url)[0]
-if table:
+table = pd.read_html(economictimes)[0]
+if not table.empty:
     df=table.set_index("Company Name")
     # Print the DataFrame
     st.table(df)
     st.divider()
     st.title("upcoming :red[Result]")
-    result= pd.read_html("https://www.livemint.com/market/quarterly-results-calendar")[0]
+    result= pd.read_html(livemint)[0]
     result.set_index('STOCKS',inplace=True)
     st.table(result)
 else:
