983e9d2dff5b5837dda052fbf3fc1911df7bd936
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2025-07-13 13:57:54 +0530

    Update IPO-GMP.py
    change according to the source website

--- pages/IPO-GMP.py
+++ pages/IPO-GMP.py
@@ -17,9 +17,9 @@ st.title('upcomming :red[IPO] and :gray[GMP]')
 try:
     # extract data from ipodekho.com using requests library
     gmp= pd.read_html('https://ipodekho.in/ipo-gmp-grey-market-premium/')[0]
-    ipo= pd.read_html('https://ipodekho.in/upcoming-ipo/')[0]
+    ipo= pd.read_html('https://ipodekho.in/')[0]
     ipo[['GMP','Profit %']]=gmp[['GMP','Profit %']]
-    ipo["Company"] = ipo["Name"].str.replace(r'(Upcomin|Live|Last|List|Allotment).*', '', regex=True)
+    ipo["Company"] = ipo["Name"].str.replace(r'(Upcomin|Live|Last|List|Waiting|Allotment).*', '', regex=True)
     ipo['Close'] = ipo['Close'].apply(pd.to_datetime,format="%d %b, %Y",errors='coerce')
     current_date= datetime.now()-timedelta(days=5) # 5 day before current date
     ipo=ipo[ipo['Close']>=current_date]
