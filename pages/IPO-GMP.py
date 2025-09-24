d07cf24282e0a1e9792c6f3b8725f266270adb89
suman sarkar <140343541+guddusarkar@users.noreply.github.com>
2025-09-24 16:08:40 +0530

    Update IPO-GMP.py
    Profit % column has been removed. due to changes in the source

--- pages/IPO-GMP.py
+++ pages/IPO-GMP.py
@@ -18,13 +18,13 @@ try:
     # extract data from ipodekho.com using requests library
     gmp= pd.read_html('https://ipodekho.in/ipo-gmp-grey-market-premium/')[0]
     ipo= pd.read_html('https://ipodekho.in/')[0]
-    ipo[['GMP','Profit %']]=gmp[['GMP','Profit']]
+    ipo[['GMP']]=gmp[['GMP']]
     ipo["Company"] = ipo["IPO Name"].str.replace(r'(Upcomin|Live|Last|List|Waiting|Allotment).*', '', regex=True)
     ipo['Close'] = ipo['Close'].apply(pd.to_datetime,format="%d %b, %Y",errors='coerce')
     current_date= datetime.now()-timedelta(days=5) # 5 day before current date
     ipo=ipo[ipo['Close']>=current_date]
     ipo['Close'] = ipo['Close'].dt.strftime('%d %b, %Y')
-    df=ipo[["Company","Type","Open","Close","GMP","Profit %","Lot Price"]]#.set_index('Company')
+    df=ipo[["Company","Type","Open","Close","GMP","Lot Price"]]#.set_index('Company')
     df = df.sort_values(by="Close", ascending=False)
     # Print the DataFrame streamlit page
     st.dataframe(df,hide_index= True)
